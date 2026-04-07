12e8fa81639cee25bb0e120eec96d128ccb14111
SebassG05 <sgandiagutierrez@adaits.es>
2026-04-07 11:24:07 +0200

    commit Cargar DataSet

---
+++ ProyectoRegresion/proyectoRegresion.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+# 1. CARGA DEL DATASET
+
+df = pd.read_csv(
+    'contenidoCSV/data.csv',
+    encoding='latin-1'
+)
+
+print("=== CARGA DEL DATASET ===")
+print(f"Filas: {df.shape[0]} | Columnas: {df.shape[1]}")
+
+print("\nPrimeras filas:")
+print(df.head())
+
+print("\nTipos de datos:")
+print(df.dtypes)
